781dbd184544d51d86a7f91191a41a61982b968a
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-03-25 19:34:47 +0900

    openwifiInfo 도메인 생성

---
+++ src/main/java/com/example/findopenwifi/domain/model/OpenWifiInfo.java
@@ -0,0 +1,29 @@
+package com.example.findopenwifi.domain.model;
+
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+public class OpenWifiInfo {
+
+    private String mgrNo;
+    private String wrdofc;
+    private String mainNm;
+    private String adres1;
+    private String adres2;
+    private String instlFloor;
+    private String instlTy;
+    private String instlMby;
+    private String svcSe;
+    private String cmcwr;
+    private String cnstcYear;
+    private String inoutDoor;
+    private String remars3;
+    private double lat;
+    private double lnt;
+    private Date workDttm;
+
+
+
+}
