d72455673c661d39b60a3dd66002ec948d54f50d
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-03-30 22:13:38 +0900

    JsonUtil getJsonObject Optional 반환 테스트 수정

--- src/main/java/com/example/findopenwifi/domain/util/JsonParsingUtilTest.java
+++ src/main/java/com/example/findopenwifi/domain/util/JsonParsingUtilTest.java
@@ -1,9 +1,13 @@
 package com.example.findopenwifi.domain.util;
 
 import com.example.findopenwifi.domain.service.OpenWifiServiceImpl;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -40,29 +44,44 @@ class JsonParsingUtilTest {
             "    }\n" +
             "}";
 
+    private static final String endJsonData = "{\n" +
+            "    \"RESULT\": {\n" +
+            "        \"CODE\": \"ERROR-336\",\n" +
+            "        \"MESSAGE\": \"데이터요청은 한번에 최대 1,000건을 넘을 수 없습니다.\\n요청종료위치에서 요청시작위치를 뺀 값이 1000을 넘지 않도록 수정하세요.\"\n" +
+            "    }\n" +
+            "}";
+
     @Test
     void 문자열로_부터_JsonObject_가져오기() {
-        JsonObject tbPublicWifiInfo = jsonParsingUtil.getJsonObject(testJson, "TbPublicWifiInfo");
+        Optional<JsonObject> tbPublicWifiInfo = jsonParsingUtil.getJsonObject(testJson, "TbPublicWifiInfo");
 
-        assertThat(tbPublicWifiInfo).isInstanceOf(JsonObject.class);
+        assertThat(tbPublicWifiInfo.get()).isInstanceOf(JsonObject.class);
     }
 
     @Test
     void list_total_count_가져오기() {
-        JsonObject tbPublicWifiInfo = jsonParsingUtil.getJsonObject(testJson, "TbPublicWifiInfo");
+        Optional<JsonObject> tbPublicWifiInfo = jsonParsingUtil.getJsonObject(testJson, "TbPublicWifiInfo");
 
-        int listTotalCount = jsonParsingUtil.getInt(tbPublicWifiInfo, "list_total_count");
+        int listTotalCount = jsonParsingUtil.getInt(tbPublicWifiInfo.get(), "list_total_count");
         assertThat(listTotalCount).isEqualTo(23406);
     }
 
     @Test
     void result_code_and_message_가져오기() {
-        JsonObject tbPublicWifiInfo = jsonParsingUtil.getJsonObject(testJson, "TbPublicWifiInfo");
-        JsonObject result = jsonParsingUtil.getJsonObject(tbPublicWifiInfo, "RESULT");
+        Optional<JsonObject> tbPublicWifiInfo = jsonParsingUtil.getJsonObject(testJson, "TbPublicWifiInfo");
+        JsonObject result = jsonParsingUtil.getJsonObject(tbPublicWifiInfo.get(), "RESULT");
         String code = jsonParsingUtil.getString(result, "CODE");
         String message = jsonParsingUtil.getString(result, "MESSAGE");
 
         assertThat(code).isEqualTo("INFO-000");
         assertThat(message).isEqualTo("정상 처리되었습니다");
+    }
+
+    @Test
+    void 데이터_범위_벗어났을때_리턴값_파싱() {
+        JsonParser.parseString(endJsonData);
+        JsonElement tbPublicWifiInfo = JsonParser.parseString(endJsonData).getAsJsonObject().get("TbPublicWifiInfo");
+
+        assertThat(tbPublicWifiInfo).isNull();
     }
 }
