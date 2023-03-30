6b341873393e1049bb928bfe39bc6a3233066af5
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-03-30 22:15:46 +0900

    /wifi/getOpenApi 시 모든 데이터 가져와서 저장 기능

--- src/main/java/com/example/findopenwifi/web/controller/OpenApiController.java
+++ src/main/java/com/example/findopenwifi/web/controller/OpenApiController.java
@@ -21,6 +21,7 @@ public class OpenApiController extends HttpServlet {
     @Override  //TODO: Post로 바꾸기
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        openApiService.saveOpenWifiInfoFromTo(1, 10);
+        int applyCount = openApiService.getAllOpenWifiData();
+        System.out.println(applyCount);
     }
 }
