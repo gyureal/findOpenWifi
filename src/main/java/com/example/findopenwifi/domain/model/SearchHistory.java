4530d12aa2178d454011ca27f0af8a8cbbf72282
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-06 00:30:41 +0900

    SearchHistory model 수정 - of 메서드 추가

--- src/main/java/com/example/findopenwifi/domain/model/SearchHistory.java
+++ src/main/java/com/example/findopenwifi/domain/model/SearchHistory.java
@@ -1,18 +1,25 @@
 package com.example.findopenwifi.domain.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class SearchHistory {
     private int id;
     private Double xValue;
     private Double yValue;
     private String searchDateTime;
     private String dataDelYn;
+
+    private SearchHistory(Double xValue, Double yValue) {
+        this.xValue = xValue;
+        this.yValue = yValue;
+    }
+
+    public static SearchHistory of(Double xValue, Double yValue) {
+        return new SearchHistory(xValue, yValue);
+    }
 }
