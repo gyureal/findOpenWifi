43e752c42409cd803df98fd541680455bc0fcf01
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-05 22:56:01 +0900

    domain searchHistory

--- src/main/java/com/example/findopenwifi/domain/model/SearchHistory.java
+++ src/main/java/com/example/findopenwifi/domain/model/SearchHistory.java
@@ -1,4 +1,9 @@
 package com.example.findopenwifi.domain.model;
 
 public class SearchHistory {
+    private int id;
+    private Double xValue;
+    private Double yValue;
+    private String searchDateTime;
+    private String dataDelYn;
 }
