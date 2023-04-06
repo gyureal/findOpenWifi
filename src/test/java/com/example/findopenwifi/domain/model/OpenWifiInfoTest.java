1600ea2185c7bc9abc4854bd01fd9ce25a0ce97c
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-06 22:14:35 +0900

    OpenWifiInfoTest 생성, 객체 생성 메서드 테스트

--- src/test/java/com/example/findopenwifi/domain/model/OpenWifiInfoTest.java
+++ src/test/java/com/example/findopenwifi/domain/model/OpenWifiInfoTest.java
@@ -1,12 +1,29 @@
 package com.example.findopenwifi.domain.model;
 
+import com.example.findopenwifi.domain.dto.RawWifiInfoObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class OpenWifiInfoTest {
 
     @Test
-    void test() {
+    void create_test() {
 
+        String mgrNo = "test11";
+        double lat = 2.33;
+        double lnt = 3.224;
 
+        RawWifiInfoObject dto = new RawWifiInfoObject();
+        dto.setX_SWIFI_MGR_NO(mgrNo);
+        dto.setLAT(lat);
+        dto.setLNT(lnt);
+
+        OpenWifiInfo openWifiInfo = OpenWifiInfo.from(dto);
+
+        assertNotNull(openWifiInfo);
+        assertEquals(mgrNo, openWifiInfo.getMgrNo());
     }
 
 }
